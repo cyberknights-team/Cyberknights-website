116fb756cc718daa5a1e94bb570a82d1ce3ac58f
logaprakash <logaprakash98@gmail.com>
2017-03-11 16:41:06 +0530

    updated developers with products

--- products.php
+++ products.php
@@ -38,40 +38,51 @@
             </div>
 			
         <hr class="uk-grid-divider">
-         <br><br><br>  
+         <br> 
                <div class="uk-grid" data-uk-grid-margin="">
         <div class="uk-width-medium-1-2 uk-row-first">
 		  <h1>PLoT</h1>
 					
 			<h3>Description</h3>
-				<p>
-				PLoT is a virtual team space, where the conventional way of sharing files is replaced with vector mapping of files to make it faster and even more secure to use while using much lesser space at the same time.
-				PLoT provides it's users with one of the secure platforms available out there for team collaboration with the best in house features like Geo-Fencing, Network-Fencing, Device Restriction etc. Using Device Restriction feature, the user can restrict the usage to the only registered device associated with a user. i.e. "One User Account, One Device."
-				Reducing the junk files has been the core idea of PLoT with its user as well as server friendly features making it one of the best available application for file management and the preferred options for organizations.
-				</p>
-                    </div>
+				<ul>
+				<li>PLoT is a virtual team space, where the conventional way of sharing files is replaced with vector mapping of files to make it faster and even more secure to use while using much lesser space at the same time.</li>
+				<li>PLoT provides it's users with one of the secure platforms available out there for team collaboration with the best in house features like Geo-Fencing, Network-Fencing, Device Restriction etc. </li>
+				<li>Using Device Restriction feature, the user can restrict the usage to the only registered device associated with a user. i.e. "One User Account, One Device."</li>
+				<li>Reducing the junk files has been the core idea of PLoT with its user as well as server friendly features making it one of the best available application for file management and the preferred options for organizations.</li>
+				</ul>
+				<h3>Developered by:</h3>
+				<div style="padding-left:20px;">
+				<img src="assets/developers/logaprakash.jpg" alt="logaprakash"  style = "border-radius: 50%;padding-left:20px;" height="100" width="100">
+				<img src="assets/developers/prajwal.jpg" alt="prajwal"  style = "border-radius: 50%;padding-left:20px;" height="100" width="100">
+				<img src="assets/developers/tharun.jpg" alt="tharun"  style = "border-radius: 50%;padding-left:20px;" height="100" width="100">
+                </div>
+				</div>
 
                 <div class="uk-width-medium-1-2">
-					<img src="assets/apps/plot.png" alt="" height="400" width="660">
+				<br><br>
+					<img src="assets/apps/plot.png" alt="plot" height="400" width="660">
 				<center>   
 				   <a target="_blank" class="uk-button uk-button-primary" href="download/plot">Download PLoT from Windows Store</a>				  
 				</center>
+				<br><br>
 				   <p>
 				   <b>Platform: </b>Windows 8 or above (PC)<br>
-				   <b>DISCLAIMER: </b>The app/server/author do not hold any responsibility over the content shared through this service.</p>
-			  		
+				   <b>DISCLAIMER: </b>CyberKnights are not responsible for any misuse of features and shall hold no responsibility over any misleading invites or files sent through PLoT.</p>
                 </div>
 
                </div>
+			    <br>
 			   <hr class="uk-grid-divider">
-         <br><br><br>  
+         <br>
 			   <div class="uk-grid" data-uk-grid-margin="">
                 <div class="uk-width-medium-1-2 uk-row-first">
+				<br><br>
                     <img src="assets/apps/hlin.png" alt="" height="400" width="660">
 				<center>   
 				   <a target="_blank" class="uk-button uk-button-primary" href="download/HlinWindowsDesktop">Download HLIN from Windows Store</a>
 				   <a target="_blank" class="uk-button uk-button-primary" href="http://cyberknights.in/github-hlin">View source in GitHub</a>
 				</center>
+				<br><br>
 				   <p>
 				   <b>Platform: </b>Windows 8 or above (PC)<br>
 				   <b>DISCLAIMER: </b>The app/server/author do not hold any responsibility over the content shared through this service.</p>
@@ -96,17 +107,24 @@
 					 <li>User-defined range of access in Geo-Lock ranging from 30 m - 250 m.</li>
 					 <li>Network Lock (WiFi based).</li>
 					 <li>For better safety, Access keys are deactivated after 30 days!</li>
-					<ul>
+					</ul>
 					
+					<h3>Developered by:</h3>
+				<div style="padding-left:20px;">
+				<img src="assets/developers/logaprakash.jpg" alt="logaprakash"  style = "border-radius: 50%;padding-left:20px;" height="100" width="100">
+				<img src="assets/developers/prajwal.jpg" alt="prajwal"  style = "border-radius: 50%;padding-left:20px;" height="100" width="100">
+				<img src="assets/developers/tharun.jpg" alt="tharun"  style = "border-radius: 50%;padding-left:20px;" height="100" width="100">
+				<img src="assets/developers/uddalak.jpg" alt="uddalak"  style = "border-radius: 50%;padding-left:20px;" height="100" width="100">			               
+			   </div>
 					
                 </div>
 
                </div>
 			
 
-			   <br><br><br>
+			   <br>
 			 <hr class="uk-grid-divider">
-			 <br><br><br>
+			 <br>
 			  <div class="uk-grid" data-uk-grid-margin="">
 
                 <div class="uk-width-medium-1-2 uk-row-first">
@@ -120,11 +138,15 @@
                      <li>The files, for better security are encrypted before being saved in the servers.</li>				 
                      <li>Languages used: HTML5,CSS3,JS,PHP</li>
 				  </ul>
+				  <h3>Developered by:</h3>
+				<div style="padding-left:20px;">
+				<img src="assets/developers/logaprakash.jpg" alt="logaprakash"  style = "border-radius: 50%;padding-left:20px;" height="100" width="100">
+				</div>
 			   </div>
 
                 <div class="uk-width-medium-1-2">
                    <center> 
-				   <br>
+				   <br><br><br><br>
 				   <img src="assets/apps/cyberknights_website.png" alt="" height="70" width="260">
 				   <br><br><br>
 				   <a target="_blank" class="uk-button uk-button-primary" href="https://github.com/cyberknights-team/Website">View source in GitHub</a>
@@ -136,7 +158,7 @@
 
 			 
        
-			   <br><br><br>
+			   <br>
 			   <hr class="uk-grid-divider">
 			   <br><br><br>
          <footer>
