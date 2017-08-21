8e36b9a61cc58f763de1da339c9cfb5ac1cb1b24
logaprakash <logaprakash98@gmail.com>
2017-08-21 19:24:05 +0530

    commit

--- digi-school/controller/config.php
+++ digi-school/controller/config.php
@@ -1,4 +1,3 @@
 <?php
-
 $connectionString=  "DefaultEndpointsProtocol=http;AccountName=digischool;AccountKey=sxywlh0sslKGjkox4Pv/7/bD9xy+kqKp4DwTGG0qwDfIw/WLG2PJJZtQTFzqKrRn6S2fk+T4pE6DgtRXgxMI4g==";
 ?>
