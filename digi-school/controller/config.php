e54c881dbeca8904e37da6132e244e76b17f870d
logaprakash <logaprakash98@gmail.com>
2017-08-22 11:53:51 +0530

    update

--- digi-school/controller/config.php
+++ digi-school/controller/config.php
@@ -1,3 +1,3 @@
 <?php
-$connectionString=  "DefaultEndpointsProtocol=http;AccountName=digischool;AccountKey=sxywlh0sslKGjkox4Pv/7/bD9xy+kqKp4DwTGG0qwDfIw/WLG2PJJZtQTFzqKrRn6S2fk+T4pE6DgtRXgxMI4g==";
+$connectionString=  "DefaultEndpointsProtocol=https;AccountName=digischool;AccountKey=R8YiFHNFHlOx0jVZGThFazr81FHXqCpmRBkU2SLs1RMVTf6Kec6S3oSdntxmunMIOpp4v3F7PB/whpsf74QwwQ==";
 ?>
