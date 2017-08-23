996b9fe6c4e9ba89f7a914681d858ebba7aa3317
logaprakash <logaprakash98@gmail.com>
2017-08-23 15:04:22 +0530

    update

--- digi-school/controller/deleteFeedback.php
+++ digi-school/controller/deleteFeedback.php
@@ -5,35 +5,13 @@ require_once 'vendor/autoload.php';
 use WindowsAzure\Common\ServicesBuilder;
 use MicrosoftAzure\Storage\Common\ServiceException;
 
-// Create table REST proxy.
-$tableRestProxy = ServicesBuilder::getInstance()->createTableService($connectionString);
-
-
-try    {
-    $result = $tableRestProxy->queryEntities("feedback");
-}
-catch(ServiceException $e){
-    // Handle exception based on error codes and messages.
-    // Error codes and messages are here:
-    // http://msdn.microsoft.com/library/azure/dd179438.aspx
-    $code = $e->getCode();
-    $error_message = $e->getMessage();
-    echo $code.": ".$error_message."<br />";
-}
-
-$entities = $result->getEntities();
-
-	foreach($entities as $entity){
-	if($entity->getPartitionKey()==$_POST['partition'] && $entity->getRowKey()==$_POST['rowkey']){
-
 
 // Create table REST proxy.
 $tableRestProxy = ServicesBuilder::getInstance()->createTableService($connectionString);
 
 try    {
-	
     // Delete entity.
-    $tableRestProxy->deleteEntity("feedback", $entity->getPartitionKey() ,$entity->getRowKey());
+    $tableRestProxy->deleteEntity("feedback", $_POST['partition'] ,$_POST['rowkey');
 }
 catch(ServiceException $e){
     // Handle exception based on error codes and messages.
@@ -43,8 +21,8 @@ catch(ServiceException $e){
     $error_message = $e->getMessage();
     echo $code.": ".$error_message."<br />";
 }
-	}
-}
+	
+
 
 header("Location:../welcome.php?div=showFeedbacks&result=y");
 	
