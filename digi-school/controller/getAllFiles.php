e8cda89a5f2d0646dcddd339e629251cb45fc32d
logaprakash <logaprakash98@gmail.com>
2017-08-24 11:11:32 +0530

    update

---
+++ digi-school/controller/getAllFiles.php
@@ -0,0 +1,29 @@
+<?php
+
+include 'config.php';
+
+require_once 'vendor/autoload.php';
+
+use WindowsAzure\Common\ServicesBuilder;
+use MicrosoftAzure\Storage\Common\ServiceException;
+
+// Create table REST proxy.
+$tableRestProxy = ServicesBuilder::getInstance()->createTableService($connectionString);
+
+
+try    {
+    $result = $tableRestProxy->queryEntities("files");
+}
+catch(ServiceException $e){
+    // Handle exception based on error codes and messages.
+    // Error codes and messages are here:
+    // http://msdn.microsoft.com/library/azure/dd179438.aspx
+    $code = $e->getCode();
+    $error_message = $e->getMessage();
+    echo $code.": ".$error_message."<br />";
+}
+
+$entities = $result->getEntities();
+
+
+?>
