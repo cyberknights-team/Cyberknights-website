78fcf2af26130f2a2a8c77b17a9efcc242828a1f
logaprakash <logaprakash98@gmail.com>
2017-08-30 17:12:44 +0530

    Create download.php

---
+++ digi-school/controller/download.php
@@ -0,0 +1,27 @@
+<?php
+include 'config.php';
+$container = $_GET['container'];
+$file = $_GET['file'];
+require_once 'vendor/autoload.php';
+
+use WindowsAzure\Common\ServicesBuilder;
+use MicrosoftAzure\Storage\Common\ServiceException;
+
+// Create blob REST proxy.
+$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
+
+
+try    {
+    // Get blob.
+    $blob = $blobRestProxy->getBlob($container, $file);
+    fpassthru($blob->getContentStream());
+}
+catch(ServiceException $e){
+    // Handle exception based on error codes and messages.
+    // Error codes and messages are here:
+    // http://msdn.microsoft.com/library/azure/dd179439.aspx
+    $code = $e->getCode();
+    $error_message = $e->getMessage();
+    echo $code.": ".$error_message."<br />";
+}
+?>
