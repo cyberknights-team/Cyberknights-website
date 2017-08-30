cd8b7a5c96c041eb92125f642f8243fb3b929550
logaprakash <logaprakash98@gmail.com>
2017-08-30 17:14:21 +0530

    Update welcome.php

--- digi-school/welcome.php
+++ digi-school/welcome.php
@@ -386,7 +386,7 @@ foreach($entities as $entity){
 	echo "<td>".$entity->getRowKey()."</td>";
 	echo "<td>".$entity->getProperty("Contains")->getValue()."</td>";
 	echo "<center><td>
-	<a target='_blank' href='http://digischool.blob.core.windows.net/".$entity->getPartitionKey()."/".$entity->getRowKey()."'>
+	<a target='_blank' href='controller/download.php?container=".$entity->getPartitionKey()."&file=".$entity->getRowKey()."'>
 	<button type='submit' class='btn btn-info btn-fill'>Download</button>
 	</a>";
 	echo "</tr>";	
