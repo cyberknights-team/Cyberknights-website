069b6207e769c1b3dbaaa51ff080c0701b13c3d6
logaprakash <logaprakash98@gmail.com>
2017-09-01 15:35:53 +0530

    update

--- digi-school/controller/download.php
+++ digi-school/controller/download.php
@@ -1,29 +1,12 @@
 <?php
-include 'config.php';
 $container = $_GET['container'];
 $file = $_GET['file'];
-require_once 'vendor/autoload.php';
 
-use WindowsAzure\Common\ServicesBuilder;
-use MicrosoftAzure\Storage\Common\ServiceException;
-
-// Create blob REST proxy.
-$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
-
-
-try    {
-    // Get blob.
-    $blob = $blobRestProxy->getBlob($container, $file);
-    $source = stream_get_contents($blob->getContentStream());
-	$localPath = '/var/www/path/to/my/downloaded/file';
-	$result = file_put_contents($localPath, $source);
-}
-catch(ServiceException $e){
-    // Handle exception based on error codes and messages.
-    // Error codes and messages are here:
-    // http://msdn.microsoft.com/library/azure/dd179439.aspx
-    $code = $e->getCode();
-    $error_message = $e->getMessage();
-    echo $code.": ".$error_message."<br />";
-}
+$file_url = 'http://digischool.blob.core.windows.net/' .$container.'/'. $file;
+header('Content-Type: application/octet-stream');
+header("Content-Transfer-Encoding: Binary"); 
+header("Content-disposition: attachment; filename=\"".$file."\""); 
+readfile($file_url);
+exit;
 ?>
+
