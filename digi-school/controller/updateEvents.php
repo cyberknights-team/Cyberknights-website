e8b2f510e8088329bf53549641edaacfd909b828
logaprakash <logaprakash98@gmail.com>
2017-09-02 09:57:44 +0530

    update

---
+++ digi-school/controller/updateEvents.php
@@ -0,0 +1,43 @@
+<?php
+
+include 'config.php';
+
+require_once 'vendor/autoload.php';
+
+use WindowsAzure\Common\ServicesBuilder;
+use MicrosoftAzure\Storage\Common\ServiceException;
+use MicrosoftAzure\Storage\Table\Models\Entity;
+use MicrosoftAzure\Storage\Table\Models\EdmType;
+
+// Create table REST proxy.
+$tableRestProxy = ServicesBuilder::getInstance()->createTableService($connectionString);
+
+$result = $tableRestProxy->getEntity("events", $_POST['partition'], $_POST['rowKey']);
+
+$entity = $result->getEntity();
+
+$entity->setPropertyValue("ParitionKey", $_POST['eventCode']);
+
+$entity->setPropertyValue("RowKey", $_POST['accessCode']);
+
+$entity->setPropertyValue("Date", $_POST['date']);
+
+$entity->setPropertyValue("EventName", $_POST['EventName']);
+
+$entity->setPropertyValue("EventPlace", $_POST['EventPlace']);
+
+try    {
+    $tableRestProxy->updateEntity("events", $entity);
+}
+catch(ServiceException $e){
+    // Handle exception based on error codes and messages.
+    // Error codes and messages are here:
+    // http://msdn.microsoft.com/library/azure/dd179438.aspx
+    $code = $e->getCode();
+    $error_message = $e->getMessage();
+    echo $code.": ".$error_message."<br />";
+}
+
+header("Location: ../welcome.php?div=showEvents&result=y");
+
+?>
